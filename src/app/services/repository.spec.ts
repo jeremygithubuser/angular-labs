6943c21e656afa647adfc9cf0a235bd22a07a1b8
jeremy daniel bouvier <jeremydaniel.bouvier@gmail.com>
2020-02-15 19:37:47 -0500

    work in progress

--- src/app/services/repository.spec.ts
+++ src/app/services/repository.spec.ts
@@ -1,52 +1,91 @@
 // Straight Jasmine testing without Angular's testing support
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
-import {HttpClientTestingModule,HttpTestingController} from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Repository } from './repository';
 import { Observable } from 'rxjs';
 import { Post } from '../models/post';
 import { post } from 'selenium-webdriver/http';
 describe('Repository', () => {
-    let service: Repository;
 
-    /*beforeEach(() => { service = new Repository(new HttpClient()); });*/
+  const posts = [
+    {
+      userId: 1,
+      id: 1,
+      title: 'sunt aut facere repellat',
+      body: 'quia et suscipit'
+    } as Post,
+    {
+      userId: 1,
+      id: 2,
+      title: 'qui est esse',
+      body: 'est rerum tempore'
+    } as Post,
+  ];
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-          imports: [HttpClientTestingModule],
-          providers: [Repository]
-        });
-      });
-  
-    it('#getValue should return real value',
-    inject(
-        [HttpTestingController, Repository],
-        (
-          httpMock: HttpTestingController,
-          repository: Repository
-        ) => {
-            const posts = [
-                { userId: 1, id: 1 ,title: 'title01',body:'body01'} as Post,
-                { userId: 2, id: 2 ,title: 'title02',body:'body02'} as Post,
-              ];
-              
-              repository.getPosts().subscribe((_posts:Post[]) => {
-                    expect(_posts).toEqual(posts);            
-              });
-              
-              const mockReq = httpMock.expectOne(repository.postUrl);
-              
-              expect(mockReq.cancelled).toBeFalsy();
-
-              expect(mockReq.request.responseType).toEqual('json');
-              
-              mockReq.flush(posts);
-              
-              httpMock.verify();
-        }
-      )
-    );
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [Repository]
+    });
   });
+
+  it('getPosts should return the posts',
+    inject(
+      [HttpTestingController, Repository],
+      (
+        httpMock: HttpTestingController,
+        repository: Repository
+      ) => {
+
+        const expected = posts;
+        repository.getPosts().subscribe((p: Post[]) => {
+          expect(p).toEqual(expected);
+        });
+
+        const mockReq = httpMock.expectOne(repository.postUrl);
+
+        expect(mockReq.cancelled).toBeFalsy();
+
+        expect(mockReq.request.responseType).toEqual('json');
+
+        mockReq.flush(posts);
+
+        httpMock.verify();
+      }
+    )
+  );
+
+  it('getPostsWithTitlesInCapitalLetters should return the posts with the titles in capital letters',
+    inject(
+      [HttpTestingController, Repository],
+      (
+        httpMock: HttpTestingController,
+        repository: Repository
+      ) => {
+
+        const expected = posts.map(p => {
+         p.title = p.title.toUpperCase();
+         return p;
+        });
+
+        repository.getPostsWithTitlesInCapitalLetters().subscribe((p: Post[]) => {
+          expect(p).toEqual(expected);
+        });
+
+        const mockReq = httpMock.expectOne(repository.postUrl);
+
+        expect(mockReq.cancelled).toBeFalsy();
+
+        expect(mockReq.request.responseType).toEqual('json');
+
+        mockReq.flush(posts);
+
+        httpMock.verify();
+      }
+    )
+  );
+});
 
 
 
--- src/app/services/repository.ts
+++ src/app/services/repository.ts
@@ -1,57 +1,67 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, map, tap, share } from 'rxjs/operators';
 import { Post } from '../models/post';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
 @Injectable()
 export class Repository {
 
-  readonly postUrl = 'posts';  // URL to web api
+    readonly postUrl = 'posts';  // URL to web api
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  /** GET heroes from the server */
-  getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>(this.postUrl)
-      .pipe(
-        tap(heroes => this.log(`fetched posts`)),
-        catchError(this.handleError('getHeroes'))
-      ) as Observable<Post[]>;
-  }
-  /**
-   * Returns a function that handles Http operation failures.
-   * This error handler lets the app continue to run as if no error occurred.
-   * @param operation - name of the operation that failed
-   */
-  private handleError<T>(operation = 'operation') {
-    return (error: HttpErrorResponse): Observable<T> => {
+    /** GET Posts from the server */
+    getPosts(): Observable<Post[]> {
+        return this.http.get<Post[]>(this.postUrl)
+            .pipe(
+                tap(post => this.log(`fetched posts`)),
+                catchError(this.handleError('getPosts'))
+            ) as Observable<Post[]>;
+    }
 
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
+    getPostsWithTitlesInCapitalLetters(): Observable<Post[]> {
+        return this.http.get<Post[]>(this.postUrl)
+            .pipe(
+                share(),
+                tap(posts => this.log(`fetched posts`)),
+                map(
+                    posts => posts.map(
+                        post => {
+                            post.title = post.title.toUpperCase();
+                            return post;
+                        }
+                    )
+                ),
+                catchError(this.handleError('getPosts'))
+            ) as Observable<Post[]>;
+    }
+    /**
+     * Returns a function that handles Http operation failures.
+     * This error handler lets the app continue to run as if no error occurred.
+     * @param operation - name of the operation that failed
+     */
+    private handleError<T>(operation = 'operation') {
+        return (error: HttpErrorResponse): Observable<T> => {
 
-      const message = (error.error instanceof ErrorEvent) ?
-        error.error.message :
-       `server returned code ${error.status} with body "${error.error}"`;
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
 
-      // TODO: better job of transforming error for user consumption
-      throw new Error(`${operation} failed: ${message}`);
-    };
+            const message = (error.error instanceof ErrorEvent) ?
+                error.error.message :
+                `server returned code ${error.status} with body "${error.error}"`;
 
-  }
+            // TODO: better job of transforming error for user consumption
+            throw new Error(`${operation} failed: ${message}`);
+        };
 
-  private log(message: string) {
-    console.log('PostService: ' + message);
-  }
+    }
+
+    private log(message: string) {
+        console.log('PostService: ' + message);
+    }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
